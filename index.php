d0f69a999df60de08532e05d528d1efce30a2927
Jake Lawrence <jakelawrence@csu.fullerton.edu>
2019-11-05 14:07:34 -0800

    Formatted landing page
    The landing page now consists of the option to select the area that you are currently looking for events in, each area will take you to a different page in which it will list the events for that given area.

--- index.php
+++ index.php
@@ -3,94 +3,138 @@
 <title>FestApp</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-img {margin-bottom: -7px}
-.w3-row-padding img {margin-bottom: 12px}
-body {font-family: "Lato", sans-serif}
-.mySlides {display: none}
+
+  img {margin-bottom: -7px}
+  .w3-row-padding img {margin-bottom: 12px}
+  body {font-family: "Lato", sans-serif}
+  .mySlides {display: none}
+
+  .container {
+  position: relative;
+  width: 100%;
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+  border-radius: 5px;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: white;
+}
+
+.container:hover .overlay {
+  opacity: 0.75;
+}
+
+.text {
+  color: black;
+  font-size: 2.5em;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+
+  h1{
+    margin-left: 50px;
+  }
 </style>
 <body>
 
 <!-- Navbar -->
 <div class="w3-top">
-  <div class="w3-bar w3-black w3-card">
+  <div class="w3-bar w3-white w3-card">
     <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-    <a href="#" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-    <a href="#band" class="w3-bar-item w3-button w3-padding-large w3-hide-small">BAND</a>
-    <a href="#events" class="w3-bar-item w3-button w3-padding-large w3-hide-small">EVENTS</a>
-    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
+    <a href="index.php" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+    <a href="festivals.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">FESTIVALS</a>
+    <a href="areas.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONNECT</a>
+    <a href="" class="w3-bar-item w3-button w3-padding-large w3-hide-small">CONTACT</a>
     <div class="w3-dropdown-hover w3-hide-small">
       <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>
       <div class="w3-dropdown-content w3-bar-block w3-card-4">
-        <a href="#" class="w3-bar-item w3-button">Merchandise</a>
-        <a href="#" class="w3-bar-item w3-button">Extras</a>
-        <a href="#" class="w3-bar-item w3-button">Media</a>
+        <a href="#" class="w3-bar-item w3-button">ABOUT US</a>
+        <a href="#" class="w3-bar-item w3-button">EXTRAS</a>
+        <a href="#" class="w3-bar-item w3-button">MEDIA</a>
       </div>
     </div>
-    <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+  <a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
   </div>
 </div>
 
-<!-- Navbar on small screens (remove the onclick attribute if you want the navbar to always show on top of the content when clicking on the links) -->
-<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
-  <a href="#band" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">BAND</a>
-  <a href="#tour" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">TOUR</a>
-  <a href="#contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">CONTACT</a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
+<!-- Logo -->
+<div class="logo"style="margin-top:100px;">
+  <h1>FestApp</h1>
 </div>
 
-<!-- Page content -->
-<div class="w3-content" >
-
-  <div class="container">
 
 
-  <!-- Automatic Slideshow Images -->
-    <div class="mySlides w3-display-container w3-center">
-      <img src="https://edmmaniac.com/wp-content/uploads/2019/05/CalderWilsonEDC19.jpeg" style="width:100%">
-      <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        <h3>Electric Daisy Carnival</h3>
-        <p><b>Las Vegas, Nevada</b></p>
 
-      </div>
-      <button class="w3-button w3-white w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-      <button class="w3-button w3-white w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+<!-- Area Select -->
+<div class="w3-container"style="margin-top:50px;">
+  <div class="row">
+    <div class="column">
+      <a href="bay_area.php"><div class="container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/GoldenGateBridge-001.jpg" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">Bay Area</div>
+          </div>
+      </div></a>
     </div>
-    <div class="mySlides w3-display-container w3-center">
-      <img src="https://www.youredm.com/wp-content/uploads/2017/10/countdown-nye_alive-coverage-for-insomniac.jpg" style="width:100%">
-      <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        <h3>Countdown</h3>
-        <p><b>San Bernadino, California</b></p>
-      </div>
-      <button class="w3-button w3-white w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-      <button class="w3-button w3-white w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+    <div class="column">
+      <a href="LA.php"><div class="container">
+        <img src="https://www.discoverlosangeles.com/sites/default/files/styles/hero/public/images/2019-02/DT_Skyline_01_L_0418.jpg?itok=NlvIQDy9" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">Los Angeles</div>
+          </div>
+      </div></a>
     </div>
-    <div class="mySlides w3-display-container w3-center">
-      <img src="https://www.magneticmag.com/.image/t_share/MTU5ODQyMDIwMTYzMDY5MDg1/sg17_jorgphoto_jp2_1844-copy.jpg" style="width:100%">
-      <div class="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        <h3>Snowglobe</h3>
-        <p><b>Lake Tahoe, California</b></p>
-      </div>
-      <button class="w3-button w3-white w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-      <button class="w3-button w3-white w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
+    <div class="column">
+      <a href="SD.php"><div class="container">
+        <img src="https://images.unsplash.com/photo-1519954352454-2d5a7353e277?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">San Diego</div>
+          </div>
+      </div></a>
     </div>
-
   </div>
-  <!-- The Tour Section -->
- <div class="w3-black" id="events">
-   <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
-     <h2 class="w3-wide w3-center">Upcoming Events</h2>
-        <div id='table-container'></div>
+</div>
 
 
-      </div>
 
-     </div>
-   </div>
+
+
 
 
    <script type="text/javascript" src="js/jquery.min.js"></script>
@@ -101,40 +145,8 @@ body {font-family: "Lato", sans-serif}
    <script type="text/javascript" src="js/csv_to_html_table.js"></script>
 
 <script>
-// Automatic Slideshow - change image every 4 seconds
-var slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
-function format_link(link){
-  if (link)
-  return "<a href='" + link + "' target='_blank'>" + link + "</a>";
-  else
-    return "";
-  }
-
-  CsvToHtmlTable.init({
-    csv_path: '/data/events.csv',
-    element: 'table-container',
-    allow_download: false,
-    csv_options: {separator: ',', delimiter: '"'},
-    datatables_options: {"paging": false},
-    custom_formatting: [[4, format_link]]
-      });
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function myFunction() {
@@ -148,14 +160,9 @@ function myFunction() {
 
 
 
-// When the user clicks anywhere outside of the modal, close it
-var modal = document.getElementById('ticketModal');
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
 </script>
 
 </body>
 </html>
+
