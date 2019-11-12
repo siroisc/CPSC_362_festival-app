1d9e0fc8bade2bc3b5fefabf5561a6887402789d
siroisc <dunkhi@csu.fullerton.edu>
2019-11-12 13:15:14 -0800

    Apache, mySQL, myPHP

---
+++ www/hello.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>First PHP Page</title>
+</head>
+<body>
+<?php
+echo '<h1>This better fucking work...</h1>';
+echo '<h2>FUCK MY TANAKA....</h2>';
+?>
+</body>
+</head>
+<titl
+</html>
---
+++ www/iluvmommy.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1 style="background-color:DodgerBlue;">I Love Mommy</h1>
+<p style="background-color:DodgerBlue;">Beacuse she is amazing</p>
+<h2 style="background-color:Tomato;">I love my dad</h2>
+<p style="background-color:Tomato;">Because he is a good man underneath the addiction</p>
+</body>
+</html>
