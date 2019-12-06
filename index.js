3e8c7df9e68a0265d8c235165258662cb563b50b
gemperline <agemperline@csu.fullerton.edu>
2019-12-05 18:56:22 -0800

    login form update [in progress]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function login() {
-  var email = document.getElementById('email_login').value;
-  var password = document.getElementById('password_login').value;
+  var email = document.getElementById('usrname').value;
+  var password = document.getElementById('psw').value;
   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -29,24 +29,32 @@ function register(){
 
 function showLoginModal() {
   document.getElementById('loginModal').style.display='block';
-  document.getElementById('registerModal').style.display='none'
+  document.getElementById('registerModal').style.display='none';
 }
 function showRegisterModal(){
-  document.getElementById('registerModal').style.display='block'
+  document.getElementById('registerModal').style.display='block';
   document.getElementById('loginModal').style.display='none';
 }
 
+//function.closeLoginModal() {
+//  document.getElementById('loginModal').style.display='none';
+//  document.getElementById('loginModal').style.display='none';
+//}
+
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
   var modal = document.getElementById('loginModal');
+
+window.onclick = function(event) {
+
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
 
-window.onclick = function(event) {
   var modal = document.getElementById('registerModal');
+window.onclick = function(event) {
+
   if (event.target == modal) {
     modal.style.display = "none";
   }
